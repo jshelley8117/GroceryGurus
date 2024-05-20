d5ca7eefb4edc75bffa1e5244d94b699c5f89621
joshshelley <joshuashelley81@gmail.com>
2024-05-20 12:49:16 -0500

    init project

---
+++ backend/src/test/java/com/JERZ/GroceryGurus/GroceryGurusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JERZ.GroceryGurus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryGurusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
